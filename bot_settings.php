19357e8f05b9d3b250dde646eb802b399b7975f3
birdwifi <kenji10254@gmail.com>
2018-04-25 12:51:47 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('Uc15a58d8d7d232605720f7e8b7c39731','กรอก ค่า Channel ID');
+define('d3f8893435dce20a236a58b6fa3d098a','กรอกค่า Channel secret');
+define('euPm/280DWxqISBt4QH2LuZM3ySRQWNgVqB6pWCk9TQzo9564lJn3FzPNf/jnmteKMjVqkKmtQ2P67MyLUqsS/W0Qwr6ZT8TLw4nGg7moOX/FAuP+MKpbMx7iXzyZhRvr3407z7FYNhZwxwaWCTPuQdB04t89/1O/w1cDnyilFU=','กรอกค่า Channel access token');
+?>
