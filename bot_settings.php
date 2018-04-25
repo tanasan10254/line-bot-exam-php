1b75f5822e7580a616f70a67434d7021e03d12ba
birdwifi <kenji10254@gmail.com>
2018-04-25 12:57:32 +0700

    Update bot_settings.php

--- bot_settings.php
+++ bot_settings.php
@@ -1,6 +1,6 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('Uc15a58d8d7d232605720f7e8b7c39731','กรอก ค่า Channel ID');
-define('d3f8893435dce20a236a58b6fa3d098a','กรอกค่า Channel secret');
-define('euPm/280DWxqISBt4QH2LuZM3ySRQWNgVqB6pWCk9TQzo9564lJn3FzPNf/jnmteKMjVqkKmtQ2P67MyLUqsS/W0Qwr6ZT8TLw4nGg7moOX/FAuP+MKpbMx7iXzyZhRvr3407z7FYNhZwxwaWCTPuQdB04t89/1O/w1cDnyilFU=','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','Uc15a58d8d7d232605720f7e8b7c39731');
+define('LINE_MESSAGE_CHANNEL_SECRET','d3f8893435dce20a236a58b6fa3d098a');
+define('LINE_MESSAGE_ACCESS_TOKEN','euPm/280DWxqISBt4QH2LuZM3ySRQWNgVqB6pWCk9TQzo9564lJn3FzPNf/jnmteKMjVqkKmtQ2P67MyLUqsS/W0Qwr6ZT8TLw4nGg7moOX/FAuP+MKpbMx7iXzyZhRvr3407z7FYNhZwxwaWCTPuQdB04t89/1O/w1cDnyilFU=');
 ?>
