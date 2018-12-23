53e02dd25ffe145b9d8614093d9120dac0dae532
birdwifi <kenji10254@gmail.com>
2018-12-23 08:32:43 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -58,7 +58,7 @@ if (strpos($_msg, 'สอน') !== false) {
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";    
-    $arrPostData['messages'][0]['file'] = 'https://drive.google.com/drive/folders/1kkK1umiwByGeymITrVsXftK-Nk5yASPN?usp=sharing';
+    $arrPostData['messages'][0]['text'] = 'https://drive.google.com/drive/folders/1kkK1umiwByGeymITrVsXftK-Nk5yASPN?usp=sharing';
   }
 }
 
